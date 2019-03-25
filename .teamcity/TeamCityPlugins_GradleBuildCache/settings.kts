36f8aab10dc56c5e40dcecba2178444d1fe08dba
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2019-03-25 16:06:13 +0000

    Update settings to use TeamCity 2018.2 DSL

--- .teamcity/TeamCityPlugins_GradleBuildCache/settings.kts
+++ .teamcity/TeamCityPlugins_GradleBuildCache/settings.kts
@@ -1,27 +1,28 @@
 package TeamCityPlugins_GradleBuildCache
 
-import jetbrains.buildServer.configs.kotlin.v2017_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2017_2.CheckoutMode
-import jetbrains.buildServer.configs.kotlin.v2017_2.Template
-import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.v2017_2.project
-import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.VersionedSettings
-import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.versionedSettings
-import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2017_2.vcs.GitVcsRoot
-import jetbrains.buildServer.configs.kotlin.v2017_2.version
+import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_2.CheckoutMode
+import jetbrains.buildServer.configs.kotlin.v2018_2.Template
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.project
+import jetbrains.buildServer.configs.kotlin.v2018_2.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v2018_2.projectFeatures.versionedSettings
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2018_2.version
 
-version = "2017.2"
+version = "2018.2"
+
 project {
     uuid = "8ea62758-8300-4649-9118-ce67379ce790"
-    id = "TeamCityPlugins_GradleBuildCache"
-    parentId = "TeamCityPlugins"
+    id("TeamCityPlugins_GradleBuildCache")
+    parentId("TeamCityPlugins")
     name = "Gradle Build Cache"
 
     val vcsId = "TeamCityPlugins_GradleBuildCache_GradleBuildCache"
     val vcsRoot = GitVcsRoot({
         uuid = "81b63059-b4bd-4714-b9a7-5c169039423a"
-        id = vcsId
+        id(vcsId)
         name = "gradle-build-cache"
         url = "https://github.com/rodm/teamcity-gradle-build-cache-plugin.git"
         useMirrors = false
@@ -41,7 +42,7 @@ project {
 
     val buildTemplate = Template({
         uuid = "1a61b842-422b-4f54-9f7e-435d0b646b36"
-        id = "TeamCityPlugins_GradleBuildCache_Build"
+        id("TeamCityPlugins_GradleBuildCache_Build")
         name = "build"
 
         vcs {
@@ -83,17 +84,17 @@ project {
     template(buildTemplate)
 
     val build = BuildType({
-        template(buildTemplate)
+        templates(buildTemplate)
         uuid = "320aa7eb-8962-47dd-bebf-5997a5b2afdc"
-        id = "TeamCityPlugins_GradleBuildCache_BuildTeamCity100"
+        id("TeamCityPlugins_GradleBuildCache_BuildTeamCity100")
         name = "Build - TeamCity 10.0"
     })
     buildType(build)
 
     val reportCodeQuality = BuildType({
-        template(buildTemplate)
+        templates(buildTemplate)
         uuid = "b769f56a-37a7-460b-9ebc-53ab556ea23f"
-        id = "TeamCityPlugins_GradleBuildCache_ReportCodeQuality"
+        id("TeamCityPlugins_GradleBuildCache_ReportCodeQuality")
         name = "Report - Code Quality"
 
         params {
