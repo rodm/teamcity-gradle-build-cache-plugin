b3f6ff895e562a00951f3660965d35736a3794e4
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2019-03-25 16:21:20 +0000

    Convert settings to the portable format

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,12 +1,9 @@
-package TeamCityPlugins_GradleBuildCache
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2018_2.CheckoutMode
 import jetbrains.buildServer.configs.kotlin.v2018_2.Template
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2018_2.project
-import jetbrains.buildServer.configs.kotlin.v2018_2.projectFeatures.VersionedSettings
-import jetbrains.buildServer.configs.kotlin.v2018_2.projectFeatures.versionedSettings
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2018_2.version
@@ -14,14 +11,9 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.version
 version = "2018.2"
 
 project {
-    uuid = "8ea62758-8300-4649-9118-ce67379ce790"
-    id("TeamCityPlugins_GradleBuildCache")
-    parentId("TeamCityPlugins")
-    name = "Gradle Build Cache"
 
-    val vcsId = "TeamCityPlugins_GradleBuildCache_GradleBuildCache"
+    val vcsId = "GradleBuildCache"
     val vcsRoot = GitVcsRoot({
-        uuid = "81b63059-b4bd-4714-b9a7-5c169039423a"
         id(vcsId)
         name = "gradle-build-cache"
         url = "https://github.com/rodm/teamcity-gradle-build-cache-plugin.git"
@@ -29,20 +21,8 @@ project {
     })
     vcsRoot(vcsRoot)
 
-    features {
-        versionedSettings {
-            id = "PROJECT_EXT_8"
-            mode = VersionedSettings.Mode.ENABLED
-            rootExtId = vcsId
-            showChanges = true
-            settingsFormat = VersionedSettings.Format.KOTLIN
-            buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_SETTINGS_FROM_VCS
-        }
-    }
-
     val buildTemplate = Template({
-        uuid = "1a61b842-422b-4f54-9f7e-435d0b646b36"
-        id("TeamCityPlugins_GradleBuildCache_Build")
+        id("Build")
         name = "build"
 
         vcs {
@@ -85,16 +65,14 @@ project {
 
     val build = BuildType({
         templates(buildTemplate)
-        uuid = "320aa7eb-8962-47dd-bebf-5997a5b2afdc"
-        id("TeamCityPlugins_GradleBuildCache_BuildTeamCity100")
+        id("BuildTeamCity100")
         name = "Build - TeamCity 10.0"
     })
     buildType(build)
 
     val reportCodeQuality = BuildType({
         templates(buildTemplate)
-        uuid = "b769f56a-37a7-460b-9ebc-53ab556ea23f"
-        id("TeamCityPlugins_GradleBuildCache_ReportCodeQuality")
+        id("ReportCodeQuality")
         name = "Report - Code Quality"
 
         params {
